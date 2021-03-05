40a73eb56cc8435fe1408ad93bc15ac23935df64
kodeape <sarargh@stud.ntnu.no>
2021-03-05 12:18:00 +0100

    Finished the code
    Added the usable ADC data, the bode csv file, a bode plotting script, and a plot directory to save all the plots in. I also finished the code for analyzing the ADC data, which now plots different signals and calculates SNR and Doppler speed.

--- raspi_analyze.py
+++ raspi_analyze.py
@@ -1,10 +1,6 @@
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import scipy.signal as signal
-
 
 def raspi_import(path, channels=5):
     """
@@ -19,46 +15,97 @@ def raspi_import(path, channels=5):
         data = data.reshape((-1, channels))
     return sample_period, data
 
-print("Nothing has happened yet.")
-
 # Import data from bin file
-sample_period, data = raspi_import('adcData.bin', 3)
+sample_period, data = raspi_import('adcData_good/n13.bin', 5)
 
 data = signal.detrend(data, axis=0)  # removes DC component for each channel
 sample_period *= 1e-6  # change unit to micro seconds
 
 # Decide interval of samples to look at
 smp_start = 5000
-smp_end = 5250
+smp_end = 7000
 N = smp_end - smp_start
-
 
 # Generate time axis
 num_of_samples = data.shape[0]  # returns shape of matrix
 t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 
-# Generate frequency axis and take FFT
+# Generate frequency axis
 freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
-spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
 
-# Plot the results in two subplots
-# NOTICE: This lazily plots the entire matrixes. All the channels will be put into the same plots.
-# If you want a single channel, use data[:,n] to get channel n
+# Function for plotting time-domain signal
+def plot_time(data, time, sample_start = 0, sample_end = -1):
+    fig = plt.subplot(2, 1, 1)
+    plt.title("Time domain signal")
+    plt.xlabel("Time [us]")
+    plt.ylabel("Voltage")
+    plot = plt.plot(time[sample_start:sample_end], data[sample_start:sample_end])
+    return fig
 
-plt.subplot(2, 1, 1)
-plt.title("Time domain signal")
-plt.xlabel("Time [us]")
-plt.ylabel("Voltage")
-plt.plot(t[smp_start:smp_end], data[smp_start:smp_end])
+# Function for plotting power spectrum
+def plot_power(data, freq):
+    spectrum = np.fft.fft(data, axis=0)
+    spectrum[0] = 0
+    fig = plt.subplot(2, 1, 2)
+    plt.title("Power spectrum of signal")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Power [dB]")
+    plot = plt.plot(freq, 20*np.log10(np.abs(spectrum))) # get the power spectrum
+    plt.xlim(-400, 400)
+    return fig
 
-plt.subplot(2, 1, 2)
-plt.title("Power spectrum of signal")
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Power [dB]")
-plt.plot(freq, 20*np.log(np.abs(spectrum))) # get the power spectrum
+# Plotting in-phase and quadrature
+channel_I = 2
+channel_Q = 4
+signal_I = np.array(data[:,channel_I])
+signal_Q = np.array(data[:,channel_Q])
 
-plt.show()
-print("Plot shown.")
+time_fig = plot_time(signal_I, t, smp_start, smp_end)
+power_fig = plot_power(signal_I, freq)
+plt.savefig("plots/signal_I.png")
+print()
+print("In-phase plot saved in plots/signal_I.png.")
+print()
+time_fig.clear()
+power_fig.clear()
+
+time_fig = plot_time(signal_Q, t, smp_start, smp_end)
+power_fig = plot_power(signal_Q, freq)
+plt.savefig("plots/signal_Q.png")
+print()
+print("Quadrature plot saved in plots/signal_Q.png.")
+print()
+
+time_fig = plot_time(signal_I, t, smp_start, smp_end)
+power_fig = plot_power(signal_I, freq)
+plt.savefig("plots/signal_IQ.png")
+print("I&Q plot saved in plots/signal_IQ.png.")
+time_fig.clear()
+power_fig.clear()
+
+# Generate complex spectrum
+x = signal_I + 1j*signal_Q
+spec = np.fft.fft(x, axis=0)
+spec = 20*np.log10(np.abs(spec))
+
+# Plot complex spectrum
+power_fig = plot_power(x, freq)
+plt.savefig("plots/signal_x.png")
+power_fig.clear()
+
+# Calculate SNR
+max_spec_i = np.argmax(spec)
+avg_noise = np.mean(spec[2000:2100])
+snr = spec[max_spec_i] - avg_noise
+print()
+print("SNR:", snr)
+
+# Calculate Doppler speed
+doppler_shift = len(spec) - max_spec_i
+doppler_res = 32000.0 / len(t)
+doppler_freq = doppler_res * doppler_shift
+doppler_speed = doppler_freq/160.0
+print("Estimated speed:", round(doppler_speed, 4), "m/s")
 
 ''' Tied to lab 1 & 2
 # Find time delays between microphone signals
@@ -103,3 +150,5 @@ print("Vinkel [Rad]:")
 print(angle)
 print()
 '''
+
+print()
